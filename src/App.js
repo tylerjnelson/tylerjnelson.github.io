023539bc4a6215eb1503f8ec291c08d596eaebe1
tylerjnelson <132235198+tylerjnelson@users.noreply.github.com>
2023-04-30 20:50:40 -0700

    update main with new page
    building blocks for the index page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Portfolio</h1>
       </header>
+      <div className="container">
+        <h2>My Work</h2>
+        <div className="media-container">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/maJKKSTarUU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <img src="https://i.pximg.net/img-master/img/2014/10/01/21/37/09/41829678_p0_master1200.jpg" alt="Random Image" />
+          <img src="https://i.pximg.net/img-master/img/2014/10/01/21/37/09/41829678_p0_master1200.jpg" alt="Random Image" />
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/maJKKSTarUU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <img src="https://i.pximg.net/img-master/img/2014/10/01/21/37/09/41829678_p0_master1200.jpg" alt="Random Image" />
+        </div>
+      </div>
     </div>
   );
 }
